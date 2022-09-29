b4f52b9abef818f7e389af977cb6f47e791e0fdc
laurybeth <laurybeth@msn.com>
2022-09-29 13:27:14 -0500

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
 console.log(validator);
+document
+  .getElementById("validateButton")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let cardNumber2=document.getElementById("cardNumber").value;
+   
+    let saludo = validator.isValid(cardNumber);
+    validator.maskify(cardNumber);
+    
+    alert("Mensaje de index.js\n\n"+saludo+"  "+cardNumber2);
+
+     })
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
 const validator = {
-  // ...
+  
+  isValid:function(creditCardNumber){
+
+
+    return "retorno de validator.js";
+
+  },
+
+  maskify:function(creditCardNumber){
+ 
+ 
+  }
+
 };
 
 export default validator;
